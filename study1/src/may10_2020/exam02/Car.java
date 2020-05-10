c3941e461db451084102bb501be9eaa098feb58b
Dodie Joo <dodi258@naver.com>
2020-05-11 01:14:45 +0900

    클래스
     클래스의 용도
             1. 라이브러리용 api: Application Program Interface
              2. 실행용: 프로그램의 실행 진입점인 main()메소드를 제공하는 역할

---
+++ study1/src/may10_2020/exam02/Car.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package may10_2020.exam02;
+
+/**
+ *
+ * @author 주세인1
+ */
+public class Car {
+    // 필드
+    String company = "Hyundai"; 
+    String model = "Sonata"; 
+    String color = "red"; 
+    int maxSpeed = 350; 
+    int speed; 
+}
---
+++ study1/src/may10_2020/exam02/CarExample.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package may10_2020.exam02;
+
+/**
+ *
+ * @author 주세인1
+ */
+public class CarExample {
+    public static void main(String[] args) {
+        Car myCar = new Car(); 
+        
+        // 필드값 변경
+        myCar.speed = 60; 
+    }
+}
